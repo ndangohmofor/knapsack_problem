ca01a4ea3734cd9b23e40489c850e2f25b91b3e1
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-14 22:24:33 -0500

    Added code and test for solution using dynamic programming

--- main.py
+++ main.py
@@ -17,6 +17,19 @@ def knapsack_recursive(capacity, weights, values, n):
         return max(included, excluded)
 
 
+def knapsack_dp(capacity, weights, values, n):
+    dp = [[0 for x in range(capacity + 1)] for x in range(n + 1)]
+
+    # Build table dp[][] in bottom-up manner
+    for i in range(1, n + 1):
+        for w in range(1, capacity + 1):
+            if weights[i - 1] <= w:
+                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w])
+            else:
+                dp[i][w] = dp[i - 1][w]
+    return dp[n][capacity]
+
+
 # Example usage
 values1 = [60, 100, 120]  # Values of the items
 weights1 = [10, 20, 30]  # Weights of the items
@@ -29,3 +42,4 @@ capacity2 = 5
 n2 = len(values2)
 
 print(knapsack_recursive(capacity2, weights2, values2, n2))
+print(knapsack_dp(capacity2, weights2, values2, n2))
